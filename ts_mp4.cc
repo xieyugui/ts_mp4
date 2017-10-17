528af5526c2f024aba38967adab787322ffa66c5
xieyugui <xieyugui@migu.cn>
2017-10-17 11:20:21 +0800

    test mp4 range request

--- ts_mp4.cc
+++ ts_mp4.cc
@@ -135,8 +135,10 @@ TSRemapDoRemap(void * /* ih ATS_UNUSED */, TSHttpTxn rh, TSRemapRequestInfo *rri
   // remove range
   range_field = TSMimeHdrFieldFind(rri->requestBufp, rri->requestHdrp, TS_MIME_FIELD_RANGE, TS_MIME_LEN_RANGE);
   if (range_field) {
-    TSMimeHdrFieldDestroy(rri->requestBufp, rri->requestHdrp, range_field);
-    TSHandleMLocRelease(rri->requestBufp, rri->requestHdrp, range_field);
+      TSDebug(PLUGIN_NAME, "TSRemapDoRemap range request");
+      return TSREMAP_NO_REMAP;
+//    TSMimeHdrFieldDestroy(rri->requestBufp, rri->requestHdrp, range_field);
+//    TSHandleMLocRelease(rri->requestBufp, rri->requestHdrp, range_field);
   }
 
   mc    = new Mp4Context(start, end);
